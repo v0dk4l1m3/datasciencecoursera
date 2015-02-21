db26bc5a5fa2e6cf117e5ac3daf9260887e7e893
v0dk4l1m3 <marowen.ng@gmail.com>
2015-02-20 23:26:46 -0800

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,36 @@
+## The first function will create a matrix
+## The matrix has to be square and invertible
+## You can define it like the following example:
+## x <- makeCacheMatrix(matrix(c(1,2,3,4),nrow=2,ncol=2))
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+## This function will compute the inverse of your matrix
+## cacheSolve(x) will return the inverse of the matrix
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
